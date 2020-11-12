6b6ca6ba393362f5cc49a1f8caa9c1ced5918beb
Eric D. Torres <etorresnotary@gmail.com>
2020-11-12 00:10:17 -0600

    updates

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,42 +7,20 @@ module.exports = function (app) {
     });
   });
 
-  app.post("/api/workouts", async (req, res) => {
-    try {
-      const response = await db.Workout.create({ type: "workout" });
-      res.json(response);
-    } catch (err) {
-      console.log("This error occurred creating a workout: ", err);
-    }
-  });
-
-  app.put("/api/workouts/:id", ({ body, params }, res) => {
-    const workoutID = params.id;
-    let savedExercises = [];
-    db.Workout.find({ _id: workoutID }).then((dbWorkout) => {
-      savedExercises = dbWorkout[0].exercises;
-      res.json(dbWorkout[0].exercises);
-      let allExercises = [...savedExercises, body];
-      console.log(allExercises);
-      updateWorkout(workoutID, allExercises);
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({}).then((newWorkout) => {
+      res.json(newWorkOut);
     });
   });
 
-  function updateWorkout(workoutID, exercises) {
-    db.Workout.findByIdAndUpdate(
-      workoutID,
-      { exercises: exercises },
-      (err, data) => {
-        if (err) {
-          console.log(err);
-        }
+  app.put("/api/workouts/:id", ({ params, body }, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { exercises: body } },
+      { upsert: true },
+      (updatedWorkout) => {
+        res.json(updatedWorkout);
       }
     );
-  }
-
-  app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).then((workout) => {
-      res.json(workout);
-    });
   });
 };
