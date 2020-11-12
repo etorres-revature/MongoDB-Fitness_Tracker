cd2f4ec6961fc95479848b7957b402498dbf188e
Eric D. Torres <etorresnotary@gmail.com>
2020-11-11 23:19:09 -0600

    updates to server file

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 17943;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -15,9 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness_tracker", {
   useNewUrlParser: true,
 });
+
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App is active at http://localhost:${PORT}`);
